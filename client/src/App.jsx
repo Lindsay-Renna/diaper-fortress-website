8fb41a295ca394de5fb5de40d077e46f6f37fa76
Lindsay Renna <lrenna.oct@gmail.com>
2024-08-27 15:33:55 -0400

    NavBar, routing completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import HomePage from "./Pages/HomePage";
+
 function App() {
 	return (
 		<>
-			<div className="bg-blue-500 text-white p-4">
-				<h1 className="text-2xl">Hello, Tailwind CSS in React!</h1>
-
-				<button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mt-4 rounded">
-					Click Me
-				</button>
-			</div>
+			<BrowserRouter>
+				<NavBar />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
