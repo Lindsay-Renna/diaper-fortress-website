360b948c81344002446f10c8f8dead3342327b3c
Lindsay Renna <lrenna.oct@gmail.com>
2024-08-27 11:37:47 -0400

    setting up tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	relative: true,
+	files: [
+		"./App.jsx",
+		"./pages/**/*.jsx",
+		"./components/**/*.jsx",
+		"./index.html",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
