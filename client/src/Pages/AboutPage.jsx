e6836180156581b8ed1eb6dcfbd36cd63eb14005
Lindsay Renna <lrenna.oct@gmail.com>
2024-08-29 13:06:46 -0400

    footer socials updated

--- client/src/Pages/AboutPage.jsx
+++ client/src/Pages/AboutPage.jsx
@@ -1,5 +1,16 @@
 function AboutPage() {
-	return <div>AboutPage</div>;
+	return (
+		<div id="about-page">
+			<article>
+				<h1>Our Story</h1>
+				<p>This is Ryan's super interesting story.</p>
+			</article>
+			<aside>
+				<img src="" alt="some cool image" />
+				<img src="" alt="some cool sketch" />
+			</aside>
+		</div>
+	);
 }
 
 export default AboutPage;
