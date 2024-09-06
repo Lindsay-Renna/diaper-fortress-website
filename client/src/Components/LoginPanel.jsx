2b0eba56e9c12cd7107f95217f3529e0941af1a4
Lindsay Renna <lrenna.oct@gmail.com>
2024-09-06 10:33:00 -0400

    creating authentication/login pages

---
+++ client/src/Components/LoginPanel.jsx
@@ -0,0 +1,58 @@
+function LoginPanel() {
+	return (
+		<section className="bg-gray-50 dark:bg-gray-900">
+			<div className="flex flex-col items-center justify-center px-6 py-6 mx-auto md:h-screen lg:py-0">
+				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-stone-800 dark:border-stone-700">
+					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+							Please enter your credentials
+						</h1>
+						<form className="space-y-4 md:space-y-6">
+							<div>
+								<label
+									htmlFor="email"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Username
+								</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-stone-700 dark:border-stone-600 dark:placeholder-stone-400 dark:text-white dark:focus:ring-stone-500 dark:focus:border-stone-500"
+									placeholder="username"
+									required=""
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="password"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Password
+								</label>
+								<input
+									type="password"
+									name="password"
+									id="password"
+									placeholder="••••••••"
+									className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-stone-700 dark:border-stone-600 dark:placeholder-stone-400 dark:text-white dark:focus:ring-stone-500 dark:focus:border-stone-500"
+									required=""
+								/>
+							</div>
+
+							<button
+								type="submit"
+								className="w-full border-gray-300 border-2 text-black bg-primary-600 hover:bg-stone-100 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-white"
+							>
+								Sign in
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default LoginPanel;
