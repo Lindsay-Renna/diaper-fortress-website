e92e6de7a3f2a4dd611c22239520d95932799707
Lindsay Renna <lrenna.oct@gmail.com>
2024-09-09 16:06:11 -0400

    added animations to the media page

--- client/src/Components/LoginPanel.jsx
+++ client/src/Components/LoginPanel.jsx
@@ -43,7 +43,7 @@ function LoginPanel() {
 
 							<button
 								type="submit"
-								className="w-full border-gray-300 border-2 text-black bg-primary-600 hover:bg-stone-100 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-white"
+								className="w-full border-gray-300 border-2 text-black bg-primary-600 hover:bg-stone-100 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-stone-700 dark:focus:ring-primary-800 dark:text-white"
 							>
 								Sign in
 							</button>
