c9d614450c0fe0d7450b6e337244e30a1001d304
Lindsay Renna <lrenna.oct@gmail.com>
2024-09-20 16:22:22 -0400

    setting up authentication

--- client/src/Components/LoginPanel.jsx
+++ client/src/Components/LoginPanel.jsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 function LoginPanel() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const history = useHistory();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post("http://your-server-url/login", {
+				username,
+				password,
+			});
+			localStorage.setItem("token", res.data.token);
+			history.push("/admin");
+		} catch (err) {
+			console.error("Invalid login", err);
+		}
+	};
+
 	return (
 		<section className="bg-gray-50 dark:bg-stone-900 grow items-center justify-center">
 			<div className="flex flex-col items-center justify-center px-6 py-14 mx-auto">
@@ -7,21 +29,22 @@ function LoginPanel() {
 						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
 							Please enter your credentials
 						</h1>
-						<form className="space-y-4 md:space-y-6">
+						<form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
 							<div>
 								<label
-									htmlFor="email"
+									htmlFor="username"
 									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 								>
 									Username
 								</label>
 								<input
-									type="email"
-									name="email"
-									id="email"
+									type="text"
+									name="username"
+									id="username"
 									className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-stone-700 dark:border-stone-600 dark:placeholder-stone-400 dark:text-white dark:focus:ring-stone-500 dark:focus:border-stone-500"
 									placeholder="username"
-									required=""
+									onChange={(e) => setUsername(e.target.value)}
+									required
 								/>
 							</div>
 							<div>
@@ -37,7 +60,8 @@ function LoginPanel() {
 									id="password"
 									placeholder="••••••••"
 									className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-stone-700 dark:border-stone-600 dark:placeholder-stone-400 dark:text-white dark:focus:ring-stone-500 dark:focus:border-stone-500"
-									required=""
+									required
+									onChange={(e) => setPassword(e.target.value)}
 								/>
 							</div>
 
