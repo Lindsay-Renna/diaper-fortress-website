f1d94262fd8f9807df6b11431f7691faf84b4562
Lindsay Renna <lindsayrenna.oct@gmail.com>
2024-10-28 15:11:32 -0400

    homepage now pulls data from MongoDB

--- client/src/Components/BlogCard.jsx
+++ client/src/Components/BlogCard.jsx
@@ -1,41 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 /* eslint-disable react/prop-types */
-function BlogCard({
-	imageSrcPath,
-	title,
-	date,
-	description,
-	sourceURL,
-	author,
-}) {
-	return (
-		<div className="max-w-screen-md text-white">
-			<h2 className="ml-1 mb-2 mt-4 ">
-				{date} - Written by {author}
-			</h2>
-			<div className="border-black border-solid border-2 rounded-lg flex flex-col w-full bg-zinc-800">
-				<img
-					src={
-						imageSrcPath
-							? `${imageSrcPath}`
-							: "client/public/icons/images/default-image.png"
-					}
-					alt={title}
-				/>
-				<div className="p-4">
-					<h1 className="text-3xl mb-4">{title}</h1>
-					<p className="text-xl">{description}</p>
-				</div>
-				<div className=" p-4 buttons flex gap-5 text-mytharra-purple">
-					<p>SHARE</p>
-					<Link to={sourceURL} target="blank">
-						<p>READ MORE</p>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+function BlogCard({ imageSrcPath, title, date, description, author }) {
+  const truncatedDescription =
+    description.length > 250 ? `${description.slice(0, 250)}...` : description;
+
+  return (
+    <div className="max-w-screen-lg w-full text-white">
+      <h2 className="ml-1 mb-2 mt-4 text-gray-600">
+        {date} <span className="hidden sm:inline">- Written by {author}</span>
+      </h2>
+      <div className="border-black border-solid border-2 rounded-lg flex flex-col w-full bg-zinc-800">
+        <div className="w-full overflow-hidden">
+          <img
+            className="w-full h-full"
+            src={
+              imageSrcPath
+                ? `${imageSrcPath}`
+                : 'client/public/icons/images/default-image.png'
+            }
+            alt={title}
+          />
+        </div>
+        <div className="p-4">
+          <h1 className="text-lg md:text-3xl mb-4">{title}</h1>
+          <p className="text-md md:text-xl">{truncatedDescription}</p>
+        </div>
+        <div className=" p-4 buttons flex gap-5 text-mytharra-purple">
+          <p className="hover:text-mytharra-purple-dark">SHARE</p>
+          <Link className="hover:text-mytharra-purple-dark" target="blank">
+            <p>READ MORE</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BlogCard;
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,68 +1,83 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import BlogCard from "../Components/BlogCard";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import BlogCard from '../Components/BlogCard';
 
-const API_URL = import.meta.env.VITE_API_URL;
-const devUsername = import.meta.env.VITE_DEV_USERNAME;
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 function HomePage() {
-	const [blogPosts, setBlogPosts] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+  const [blogPosts, setBlogPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [displayLimit, setDisplayLimit] = useState(5);
 
-	const getBlogPosts = async () => {
-		try {
-			const { data } = await axios.get(`${API_URL}username=${devUsername}`);
-			console.log(data);
-			setBlogPosts(data);
-			setIsLoading(false);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const getBlogPosts = async () => {
+    try {
+      const { data } = await axios.get(`${SERVER_URL}/blog/posts`);
+      console.log(data);
+      setBlogPosts(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
 
-	useEffect(() => {
-		getBlogPosts();
-	}, []);
+  const loadMorePosts = () => {
+    setDisplayLimit((prevLimit) => prevLimit + 5);
+  };
 
-	return (
-		<main className="flex-grow">
-			<div id="hero" className="relative flex min-h-screen -mt-20 -z-10">
-				<div className="absolute inset-0 bg-[url('https://cdn.mos.cms.futurecdn.net/9GTJo42N2uEr99T8Svxava.png')] bg-auto bg-no-repeat bg-center opacity-50"></div>
-				<div className="absolute inset-0 bg-black opacity-50"></div>
-				<div className="relative z-10 text-black w-full flex flex-col grow justify-center items-center text-3xl gap-8">
-					<h1 className="text-8xl drop-shadow-2xl text-white">
-						Build, Play, Thrive.
-					</h1>
-					<p className="drop-shadow-lg text-white">
-						Where Tiny Heroes Conquer Big Adventures!
-					</p>
-				</div>
-			</div>
-			<div className="flex justify-center bg-zinc-300 dark:bg-zinc-900"></div>
-			<h1 className="spicy text-center bg-zinc-50 dark:bg-zinc-900 text-[clamp(2.5rem,7vw,4rem)] pt-14 shadow-black shadow-lg">
-				From the Developer
-			</h1>
-			<div
-				id="Blog-list"
-				className="p-1 flex flex-col gap-8 items-center w-full sm:p-12 bg-zinc-50 dark:bg-zinc-900"
-			>
-				{isLoading && <h1>Loading...</h1>}
+  useEffect(() => {
+    getBlogPosts();
+  }, []);
 
-				{!isLoading &&
-					blogPosts.map((post) => (
-						<BlogCard
-							key={post.id}
-							imageSrcPath={post.cover_image}
-							title={post.title}
-							date={post.published_at.slice(0, 10)}
-							description={post.description}
-							sourceURL={post.canonical_url}
-							author={post.user.name}
-						/>
-					))}
-			</div>
-		</main>
-	);
+  return (
+    <main className="flex-grow">
+      <div id="hero" className="relative flex min-h-screen -mt-20 -z-10">
+        <div className="absolute inset-0 bg-[url('https://cdn.mos.cms.futurecdn.net/9GTJo42N2uEr99T8Svxava.png')] bg-auto bg-no-repeat bg-center opacity-50"></div>
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="relative z-10 text-black w-full flex flex-col grow justify-center items-center text-3xl gap-8">
+          <h1 className="text-8xl drop-shadow-2xl text-white">
+            Build, Play, Thrive.
+          </h1>
+          <p className="drop-shadow-lg text-white">
+            Where Tiny Heroes Conquer Big Adventures!
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center bg-zinc-300 dark:bg-zinc-900"></div>
+      <h1 className="spicy text-center bg-zinc-50 dark:bg-zinc-900 text-[clamp(2.5rem,7vw,4rem)] pt-14 shadow-black shadow-lg">
+        From the Developer
+      </h1>
+      <div
+        id="Blog-list"
+        className="p-1 flex flex-col gap-8 items-center w-full sm:p-12 bg-zinc-50 dark:bg-zinc-900"
+      >
+        {isLoading && <h1>Loading...</h1>}
+
+        {!isLoading &&
+          blogPosts
+            .slice(0, displayLimit)
+            .map((post) => (
+              <BlogCard
+                key={post.id}
+                imageSrcPath={post.coverURL}
+                title={post.title}
+                description={post.content}
+                date={post.createdAt.slice(0, 10)}
+                author="Ryan Renna"
+              />
+            ))}
+
+        {!isLoading && displayLimit < blogPosts.length && (
+          <button
+            onClick={loadMorePosts}
+            className="mt-8 px-4 py-2 bg-mytharra-purple text-white rounded-lg hover:bg-mytharra-purple-dark"
+          >
+            Load More
+          </button>
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default HomePage;
